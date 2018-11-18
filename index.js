dac1d9b98197e831ce0481d2cb482a4c49204b0d
Alex Zvorygin <grafetu@gmail.com>
2018-11-18 01:56:16 -0500

    IT'S COMING TOGETHER

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function allowDrop(ev) {
 
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-    document.getElementById(ev.target.id).style.opacity = '0.2';
+    // document.getElementById(ev.target.id).style.opacity = '0.2';
 }
 
 function drop(ev) {
@@ -16,5 +16,24 @@ function drop(ev) {
     console.log('drop', data);
     if (data === "napkin") {
         document.getElementById("gum").style.display = 'inline-block';
+    } else if (data === 'gum') {
+        document.getElementById("mathnews").style.display = 'inline-block';
+    } else if (data == 'mathnews') {
+        recycle();
     }
 }
+
+const outWithOldInWithNew = () => {
+    const old = document.getElementById('recycle')
+    old.style.display = 'none'
+    const theNew = document.getElementById('boss')
+    theNew.style.display = 'inline-block'
+}
+
+const recycle = () => {
+    console.log('recycle or die!')
+    outWithOldInWithNew()
+}
+
+setTimeout(recycle, 500)
+// recycle();
