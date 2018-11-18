a710ac8afb5f1a3e15413f1449bf07cf395cb6dc
Alex Zvorygin <grafetu@gmail.com>
2018-11-17 19:25:01 -0500

    drag and drop sort of working

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('js running')
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    document.getElementById(ev.target.id).style.opacity = '0.2';
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    console.log('drop', data);
+    if (data === "napkin") {
+        document.getElementById("gum").style.display = 'inline-block';
+    }
+}
